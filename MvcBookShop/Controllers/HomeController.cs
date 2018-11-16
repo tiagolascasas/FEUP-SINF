c94096fb7d1c33c850219357337b639ff08d400e
Beatriz de Henriques Martins <up201502858@fe.up.pt>
2018-11-16 16:38:14 +0000

    limpo das páginas extras

--- MvcBookShop/Controllers/HomeController.cs
+++ MvcBookShop/Controllers/HomeController.cs
@@ -6,38 +6,22 @@ using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using MvcBookShop.Models;
 
-namespace MvcBookShop.Controllers
-{
-    public class HomeController : Controller
-    {
-        public IActionResult Index()
-        {
-            return View();
-        }
+namespace MvcBookShop.Controllers{
 
-        public IActionResult About()
-        {
-            ViewData["Message"] = "Your application description page.";
-
-            return View();
-        }
-
-        public IActionResult Contact()
-        {
-            ViewData["Message"] = "Your contact page.";
-
-            return View();
-        }
-
-        public IActionResult Privacy()
-        {
+    public class HomeController : Controller{
+        
+        public IActionResult Index(){
             return View();
         }
 
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
-        public IActionResult Error()
-        {
-            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+
+        public IActionResult Error(){
+            return View(
+                new ErrorViewModel { 
+                    RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier 
+                }
+            );
         }
     }
 }
